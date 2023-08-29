f8a5d8dc6374d65c61e5a54858011dbf1a8f5e0d
singhvishal14 <kumarvishalsingh767@gmail.com>
2023-08-30 00:07:15 +0530

    initial commit

---
+++ Assisgnment_13/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Assisgnment_13.Models;
+
+namespace Assisgnment_13.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<TeamsAndWorldCup> twc = new List<TeamsAndWorldCup>() {
+            new TeamsAndWorldCup(){Id=1,Name="India",NoOfWorldCups=2},
+            new TeamsAndWorldCup(){Id=2,Name="Australia",NoOfWorldCups=4},
+            new TeamsAndWorldCup(){Id=3,Name="WestIndies",NoOfWorldCups=2},
+            new TeamsAndWorldCup(){Id=4,Name="England",NoOfWorldCups=1},
+
+            };
+            return View(twc);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+    }
+}
---
+++ Assisgnment_13/Models/TeamsAndWorldCup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assisgnment_13.Models
+{
+    public class TeamsAndWorldCup
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int NoOfWorldCups { get; set; }
+    }
+}
